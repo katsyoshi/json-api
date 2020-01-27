7045379f1e59948ea1ecdc3fdd663c98f0c06caa
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2020-01-28 01:12:53 +0900

    return json from diesel

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use]
-extern crate rocket;
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate rocket;
 
+use diesel::prelude::*;
+use dotenv::dotenv;
 use rocket::http::RawStr;
 use rocket_contrib::json::Json;
+use std::env;
 
-mod models;
-use models::Hello;
+pub mod schema;
+pub mod models;
+
+use models::*;
+
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    SqliteConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -19,7 +30,17 @@ fn hello(name: &RawStr) -> Json<Hello> {
     Json(Hello { hello: name.as_str().to_string(), })
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![index,hello]).launch();
+#[get("/name?<user_name>")]
+fn user(user_name: &RawStr) -> Json<User>{
+    use schema::users::dsl::*;
+    let conn = establish_connection();
+    let u = users.filter(name.eq(user_name.to_string()))
+        .filter(published.eq(true))
+        .first::<User>(&conn)
+        .expect("Error finding posts");
+    Json(u)
 }
 
+fn main() {
+    rocket::ignite().mount("/", routes![index,hello,user]).launch();
+}
--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,14 @@
 use serde::{Deserialize, Serialize};
+use diesel::Queryable;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Hello {
     pub hello: String,
+}
+
+#[derive(Queryable, Serialize, Deserialize)]
+pub struct User {
+    pub id: i32,
+    pub name: String,
+    pub published: bool,
 }
