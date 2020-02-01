44b3e5b1984e84f8268b8824a2210e255e72c873
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2020-02-01 13:21:13 +0900

    rustmft sensei

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate diesel;
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate rocket;
 
 use diesel::prelude::*;
 use dotenv::dotenv;
@@ -17,7 +19,10 @@ use models::*;
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    SqliteConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+    SqliteConnection::establish(&database_url).expect(&format!(
+        "Error connecting to {}",
+        database_url
+    ))
 }
 
 #[get("/")]
@@ -27,14 +32,15 @@ fn index() -> &'static str {
 
 #[get("/hello?wave&<name>")]
 fn hello(name: &RawStr) -> Json<Hello> {
-    Json(Hello { hello: name.as_str().to_string(), })
+    Json(Hello { hello: name.as_str().to_string() })
 }
 
 #[get("/name?<user_name>")]
-fn user(user_name: &RawStr) -> Json<User>{
+fn user(user_name: &RawStr) -> Json<User> {
     use schema::users::dsl::*;
     let conn = establish_connection();
-    let u = users.filter(name.eq(user_name.to_string()))
+    let u = users
+        .filter(name.eq(user_name.to_string()))
         .filter(published.eq(true))
         .first::<User>(&conn)
         .expect("Error finding posts");
@@ -42,5 +48,7 @@ fn user(user_name: &RawStr) -> Json<User>{
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index,hello,user]).launch();
+    rocket::ignite()
+        .mount("/", routes![index, hello, user])
+        .launch();
 }
